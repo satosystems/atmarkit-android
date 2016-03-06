751a4e6c7b3feb4176f598049e60c8fa3dfa9124
Satoshi Ogata <satosystems@gmail.com>
2016-03-07 02:11:28 +0900

    implement behavior of Application

---
+++ No02/app/src/main/java/com/example/atmarkit/no02/MainApplication.java
@@ -0,0 +1,27 @@
+package com.example.atmarkit.no02;
+
+import android.app.Application;
+import android.content.res.Configuration;
+import android.util.Log;
+
+public class MainApplication extends Application {
+    private static final String TAG = "@IT";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "Application#onCreate");
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        Log.d(TAG, "Application#onTerminate");
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Log.d(TAG, "Application#onTerminate");
+    }
+}
